b165270ae03e6a809c947b34b6dbb9fc3c8cdd5c
Mauricio Neira <mauro@up.codes>
2022-07-18 20:44:44 -0500

    Pretty print the files

--- cmd/list.go
+++ cmd/list.go
@@ -7,7 +7,10 @@ package cmd
 import (
 	"fmt"
 	"sort"
+	"strings"
 
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/jedib0t/go-pretty/v6/list"
 	"github.com/mneira10/synk/internal"
 	log "github.com/mneira10/synk/logger"
 	"github.com/mneira10/synk/s3Storage"
@@ -37,16 +40,69 @@ func printBucketFiles(s3Client s3Storage.S3Storage) {
 	listObjectsData := s3Client.ListObjects()
 	objects := listObjectsData.Contents
 
-	sort.Sort(s3Storage.ByFileName(objects))
+	// for _, object := range objects {
+	// 	fmt.Printf("%v\n", *object.Key)
+	// }
 
-	if len(objects) == 0 {
+	formatAndPrintObjects(&objects)
+
+}
+
+func formatAndPrintObjects(objects *[]types.Object) {
+	sort.Sort(s3Storage.ByFileName(*objects))
+
+	if len(*objects) == 0 {
 		fmt.Println("No files in this bucket yet!")
+		return
 	}
 
-	for _, object := range objects {
-		fmt.Printf("%v\n", *object.Key)
+	fileList := list.NewWriter()
+	fileList.SetStyle(list.StyleConnectedBold)
+
+	var fileNameStack []string
+
+	for _, object := range *objects {
+		fileName := object.Key
+
+		split := strings.Split(*fileName, "/")
+
+		for i, part := range split {
+
+			if i != len(split)-1 {
+
+				if len(fileNameStack) <= i {
+					fileList.AppendItem(part)
+					fileList.Indent()
+
+					fileNameStack = append(fileNameStack, part)
+				} else if fileNameStack[i] != part {
+
+					fileNameStack = split[:i]
+					for j := 0; j < len(split)-i; j++ {
+						fileList.UnIndent()
+					}
+
+					fileList.AppendItem(part)
+					fileList.Indent()
+					fileNameStack = append(fileNameStack, part)
+				}
+
+				continue
+			}
+
+			if len(split) == 1 {
+				for j := 0; j < len(fileNameStack); j++ {
+					fileList.UnIndent()
+				}
+				fileNameStack = []string{}
+			}
+
+			fileList.AppendItem(part)
+		}
 	}
 
+	fileList.Length()
+	fmt.Println(fileList.Render())
 }
 
 func init() {
--- go.mod
+++ go.mod
@@ -25,9 +25,11 @@ require (
 	github.com/jedib0t/go-pretty/v6 v6.3.5 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
