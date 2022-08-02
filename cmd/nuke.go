a5cc3bc5708a7ff124e79bd4c727adefa4c6d563
Mauricio Neira <mauro@up.codes>
2022-08-01 23:59:23 -0500

    Parallelize nuking

--- cmd/nuke.go
+++ cmd/nuke.go
@@ -9,11 +9,12 @@ import (
 	"os"
 
 	"github.com/mneira10/synk/internal"
+	log "github.com/mneira10/synk/logger"
 	"github.com/mneira10/synk/s3Storage"
 	"github.com/spf13/cobra"
 )
 
-const ASCII_NUKE string = `
+const ASCII_NUKE = `
       _.-^^---....,,--_
    _--                  --_
   <                        >)
@@ -25,6 +26,17 @@ const ASCII_NUKE string = `
          '-=#$%&%$#=-'
             | ;  :|
    _____.,-#%&$@%#&#~,._____`
+
+const NUM_CONCURRENT_DELETES = 10
+
+type nukeData struct {
+	bucketFilePath string
+}
+
+type nukeResultData struct {
+	nukeData nukeData
+	err      error
+}
 
 // nukeCmd represents the nuke command
 var nukeCmd = &cobra.Command{
@@ -51,15 +63,70 @@ and usage of using your command.`,
 		s3Client := s3Storage.ConfigS3(&config)
 		bucketFiles := internal.GetFilePathsInBucket(s3Client)
 
-		for _, bucketFile := range bucketFiles {
-			fmt.Println("DELETING", bucketFile)
-			s3Client.DeleteFile(bucketFile)
-			fmt.Println("DELETED", bucketFile)
+		numFilesToNuke := len(bucketFiles)
+		filesToNuke := make(chan nukeData, numFilesToNuke)
+		results := make(chan nukeResultData, numFilesToNuke)
+
+		for i := 0; i < NUM_CONCURRENT_DELETES; i++ {
+			go nukeFileWorker(filesToNuke, results, s3Client)
 		}
 
-		fmt.Println("Nuked everything in", config.BucketName)
-		fmt.Println(ASCII_NUKE)
+		for _, bucketFile := range bucketFiles {
+			nukeData := nukeData{bucketFilePath: bucketFile}
+			filesToNuke <- nukeData
+		}
+
+		close(filesToNuke)
+
+		var nukeErrorFiles []string
+
+		for i := 0; i < numFilesToNuke; i++ {
+			nukeResult := <-results
+			if nukeResult.err != nil {
+				nukeErrorFiles = append(
+					nukeErrorFiles,
+					nukeResult.nukeData.bucketFilePath,
+				)
+
+				log.WithFields(log.Fields{
+					"error": nukeResult.err,
+					"file":  nukeResult.nukeData.bucketFilePath,
+				}).Error("Could not nuke file")
+			}
+		}
+
+		if len(nukeErrorFiles) != 0 {
+			errorFilesOutput := internal.PrettifyFilePaths(&nukeErrorFiles)
+			fmt.Println("Could not nuke some files:")
+			fmt.Println(errorFilesOutput)
+			os.Exit(1)
+		} else {
+			fmt.Println("Nuked everything in", config.BucketName)
+			fmt.Println(ASCII_NUKE)
+		}
+
+		// for _, bucketFile := range bucketFiles {
+		// 	fmt.Println("DELETING", bucketFile)
+		// 	s3Client.DeleteFile(bucketFile)
+		// 	fmt.Println("DELETED", bucketFile)
+		// }
+
 	},
+}
+
+func nukeFileWorker(filesToNuke <-chan nukeData, results chan<- nukeResultData, s3Client *s3Storage.S3Object) {
+	for dataToNuke := range filesToNuke {
+		fmt.Println("DELETING", dataToNuke.bucketFilePath)
+		err := s3Client.DeleteFile(dataToNuke.bucketFilePath)
+		fmt.Println("DELETED", dataToNuke.bucketFilePath)
+
+		nukeResult := nukeResultData{nukeData: dataToNuke}
+
+		if err != nil {
+			nukeResult.err = err
+		}
+		results <- nukeResult
+	}
 }
 
 func init() {
